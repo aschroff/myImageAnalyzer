175b0c2778fa4f43b899f5aa6a4e39f68d83a470
aschroff <andreas@schroff-online.com>
2019-07-22 20:21:39 +0200

    post works

--- mvm/models.py
+++ mvm/models.py
@@ -12,18 +12,19 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    items = db.relationship('Item', backref='owner', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-class Post(db.Model):
+class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    item_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    itemname = db.Column(db.String(100), nullable=False)
+    thumbnail = db.Column(db.String(20), nullable=False, default='default.jpg')
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    description = db.Column(db.Text, nullable=False)
-    owner = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return f"Item('{self.item_file}','{self.itemname}', '{self.thumbnail}','{self.date_posted}')"
--- mvm/routes.py
+++ mvm/routes.py
@@ -1,33 +1,37 @@
-
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
 from mvm import application, db, bcrypt
-from mvm.forms import RegistrationForm, LoginForm
-from mvm.models import User, Post
+from mvm.forms import RegistrationForm, LoginForm, UpdateAccountForm, CreateItemForm
+from mvm.models import User, Item
 
 
 
 posts = [
     {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'owner': 'AndiSchroff',
+        'title': 'video1.mpg',
+        'description': 'test video',
+        'date_posted': 'July 13, 2019'
     },
     {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'owner': 'AndiSchroff',
+        'title': 'video2.mpg',
+        'description': 'test video 2',
+        'date_posted': 'July 14, 2019'
     }
 ]
+
 
 
 
 @application.route("/")
 @application.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    items = Item.query.all()
+    return render_template('home.html', items=items)
 
 
 @application.route("/about")
@@ -73,7 +77,69 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@application.route("/account")
+def save_picture(form_picture):
+    random_hex=secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picturefilename = random_hex + f_ext
+    picturepath = os.path.join(application.root_path, 'static/images/profile_pics', picturefilename)
+    outputsize = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(outputsize)
+    i.save(picturepath)
+    return picturefilename
+
+@application.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+           picturefile = save_picture(form.picture.data) 
+           current_user.image_file = picturefile
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    imagefile = url_for('static', filename='images/profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', imagefile=imagefile, form=form)
+
+def save_thumbnail(form_picture):
+    random_hex=secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picturefilename = random_hex + f_ext
+    picturepath = os.path.join(application.root_path, 'static/images/thumbnails', picturefilename)
+    outputsize = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(outputsize)
+    i.save(picturepath)
+    return picturefilename
+
+def save_item(form_picture):
+    random_hex=secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picturefilename = random_hex + f_ext
+    picturepath = os.path.join(application.root_path, 'static/images/items', picturefilename)
+    i = Image.open(form_picture)
+    i.save(picturepath)
+    return picturefilename
+
+
+@application.route("/item/new", methods=['GET', 'POST'])
+@login_required
+def item():
+    form = CreateItemForm()
+    if form.validate_on_submit():  
+        if form.item.data:
+           itemfile = save_item(form.item.data) 
+           thumbnailfile = save_thumbnail(form.item.data)  
+           print (itemfile, form.itemname.data, thumbnailfile, current_user)          
+           item = Item(item_file = itemfile, itemname = form.itemname.data, thumbnail = thumbnailfile, owner = current_user)
+           db.session.add(item)
+           db.session.commit()
+           flash('Your new item has been created', 'success')
+           return redirect(url_for('home'))
+    return render_template('create_item.html', title='New Item', form=form)
