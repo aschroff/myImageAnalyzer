6dfed8ecd93675ed7ceb67ff910680e8a3ead259
aschroff <andreas@schroff-online.com>
2019-09-07 13:17:41 +0200

    db auf neue aws angepasst

--- mvm/dbconfig.py
+++ mvm/dbconfig.py
@@ -5,5 +5,6 @@ Created on Tue Jul 16 21:16:17 2019
 @author: Andi
 """
 
-databaseURI = 'mysql://root:Welcome1@localhost/mvm'
-#databaseURI = 'mysql://dbadmin:Welcome1@aa1kym9msmvb862.cu76mq9u5srd.eu-central-1.rds.amazonaws.com/mvm'
+#databaseURI = 'mysql://root:Welcome1@localhost/mvm'
+databaseURI = 'mysql://dbadmin:Welcome1@aa185kmyt8wve44.cu76mq9u5srd.eu-central-1.rds.amazonaws.com/mvm'
+
